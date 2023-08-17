a5e6db5ac7a87d65ace348bf07094e3cd18274ba
Fungoose23 <127011545+Fungoose23@users.noreply.github.com>
2023-08-18 09:48:46 +1000

    added login page, how does one create a pretty UI elements AHhhhhh broke crap

--- app/src/main/java/com/example/acsystem/MainActivity.java
+++ app/src/main/java/com/example/acsystem/MainActivity.java
@@ -11,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_up);
     }
+    Button btn_startup_google = (Button) findViewById(R.id.btn_startup_google);
+   myButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    });
+
 }
